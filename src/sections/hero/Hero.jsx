110a25ddf6faa197bc8799e5295a578118634ce2
JohnnyBoi <ninjakai25@gmail.com>
2023-07-09 12:35:15 -0500

    add text to hero section and create about component

--- src/sections/hero/Hero.jsx
+++ src/sections/hero/Hero.jsx
@@ -4,7 +4,7 @@ export const Hero = () => {
     return (
         <>
             <section className={styles.heroContainer}>
-                <h1>Hero</h1>
+                <h1>"It is a shame for a man to grow old without seeing the beauty and strength of which his body is capable" - Socrates.</h1>
             </section>
         </>
     )
